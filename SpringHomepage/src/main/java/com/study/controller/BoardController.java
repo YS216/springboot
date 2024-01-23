a73fdf5ed254aeb3c4974290a955971995542602
YS216 <kwon0seo@gmail.com>
2024-01-23 11:33:02 +0900

    24.01.23

--- SpringHomepage/src/main/java/com/study/controller/BoardController.java
+++ SpringHomepage/src/main/java/com/study/controller/BoardController.java
@@ -5,9 +5,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.study.domain.Board;
+import com.study.domain.Member;
 import com.study.service.BoardService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class BoardController {
@@ -15,10 +20,17 @@ public class BoardController {
 	@Autowired
 	BoardService boardService;
 	
+	private Member loginUser;	// 현재 페이지에서 모두 사용하기 위해 인스턴스 변수로 선언
+	
 	@GetMapping("/list")
-	public String list(@RequestParam(value="nowPage", defaultValue="0") int nowPage, 
+	public String list(@RequestParam(value="nowPage", defaultValue="0") int nowPage,
+						HttpSession session,	// 현재 페이지에서 사용하기 위해
 						Model model) 
 	{
+		if(session.getAttribute("loginUser") != null) {
+			loginUser = (Member)session.getAttribute("loginUser");
+			model.addAttribute("loginUser", loginUser);
+		}
 														// PageRequest.of(현재페이지, 페이지당 개수[, sort])
 		model.addAttribute("boardPage", boardService.list(PageRequest.of(nowPage, 10))) ;
 		model.addAttribute("nowPage", nowPage);
@@ -26,17 +38,31 @@ public class BoardController {
 	}
 	
 	@GetMapping("/insertForm")
-	public String insertForm() {
+	public String insertForm(Model model) {
+		model.addAttribute("loginUser", loginUser);
 		return "board/insertForm";
 	}
+	
+	@PostMapping("/insert")
+	// @SessionAttribute("loginUser")는 현재 메소드에서만 사용 가능
+	public String insertForm(Board board /* @SessionAttribute("loginUser") Member member */) {
+		board.setWriter(loginUser.getId());
+		boardService.insert(board);
+		return "redirect:list";
+	}
+	
+	@GetMapping("/detailForm")
+	public String detailForm(@RequestParam("bno") Long bno, Model model) {
+		if(loginUser != null) {
+			model.addAttribute("loginUser", loginUser);
+		}
+		model.addAttribute("board", boardService.selectDetail(bno).get());
+		return "board/detailForm";
+	}
+	
+	@PostMapping("/update")
+	public String update(Board board) {
+		boardService.update(board);
+		return "redirect:list";
+	}
 }
-
-
-
-
-
-
-
-
-
-
--- SpringHomepage/src/main/java/com/study/service/BoardService.java
+++ SpringHomepage/src/main/java/com/study/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.study.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,5 +18,27 @@ public class BoardService {
 
 	public Page<Board> list(Pageable pageable) {
 		return boardRepository.findAll(pageable);
+	}
+
+	public Board insert(Board board) {
+		return boardRepository.save(board);	
+	}
+
+	public Optional<Board> selectDetail(Long bno) {
+		return boardRepository.findById(bno)
+								.map(board-> {
+									board.setCount(board.getCount() + 1);
+									return boardRepository.save(board);
+								});
+		
+	}
+
+	public Board update(Board board) {
+		// update시에는 영속성안에 board의 정보가 들어 있어야 한다
+		Board rboard = boardRepository.findById(board.getBno()).get();
+		rboard.setTitle(board.getTitle());
+		rboard.setContent(board.getContent());
+		
+		return boardRepository.save(rboard);
 	}
 }
